e2379c477f2901cba2189d1241dd37fe8a744610
Tadj <mtadjdine@yahoo.com>
2020-05-16 10:15:47 -0400

    Added a header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Component/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Tadjoudine Mouhamed
+       <Header />
     </div>
   );
 }
